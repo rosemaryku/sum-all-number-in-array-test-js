f6cb7acda315f17eb9d3b64c77982c5851cc221b
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-16 21:11:38 -0500

    Added index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+const sum = require("../sum-from-a-to-b-test");
+
+let sumSoFar = 0;
+
+function sumItems(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    if (Array.isArray(num)) {
+      sum += sumItems(num);
+    } else {
+      sum += num;
+    }
+  }
+  return sum;
 }
 
+sumItems([[1, 2, [[3], 4]], 5, []]);
+
 module.exports = sumItems;
+// console.log(sumItems([2, 5, 50]))
